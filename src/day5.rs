50007ae86d773b5966e808d8d9d93f1f6c10ed34
Glenn Griffin <Glenn Griffin ggriffiniii@gmail.com>
2025-12-05 09:17:24 -0800

    day5 improvements

--- src/day5.rs
+++ src/day5.rs
@@ -1,71 +1,76 @@
-use std::ops::RangeInclusive;
+use std::{cmp::Ordering, ops::Range};
 
 use aoc_runner_derive::aoc;
 
 #[derive(Debug)]
-struct DB {
-    fresh_ingredients: Vec<RangeInclusive<usize>>,
-    available_ingredients: Vec<usize>,
-}
-impl DB {
-    fn new(input: &str) -> DB {
-        let (fresh_input, available_input) = input.split_once("\n\n").unwrap();
-        let fresh_ingredients = fresh_input
-            .lines()
-            .map(|line| {
-                let (start, end) = line.split_once('-').unwrap();
-                start.parse().unwrap()..=end.parse().unwrap()
-            })
-            .collect();
+struct IntervalSet(Vec<Range<usize>>);
+impl IntervalSet {
+    fn new(mut ranges: Vec<Range<usize>>) -> Self {
+        ranges.sort_by_key(|r| r.start);
 
-        let available_ingredients = available_input
-            .lines()
-            .map(|line| line.parse().unwrap())
-            .collect();
-        DB {
-            fresh_ingredients,
-            available_ingredients,
+        let mut merged_tail = 0;
+        for idx in 1..ranges.len() {
+            if ranges[idx].start >= ranges[merged_tail].end {
+                merged_tail += 1;
+                ranges[merged_tail] = ranges[idx].clone();
+            } else {
+                ranges[merged_tail].end = std::cmp::max(ranges[merged_tail].end, ranges[idx].end);
+            }
         }
+        ranges.truncate(merged_tail+1);
+        IntervalSet(ranges)
+    }
+
+    fn contains(&self, needle: usize) -> bool {
+        self.0.binary_search_by(|range| {
+            match (range.start.cmp(&needle), range.end.cmp(&needle)) {
+                (Ordering::Less | Ordering::Equal, Ordering::Greater) => Ordering::Equal,
+                (Ordering::Greater, _) => Ordering::Greater,
+                (_, Ordering::Less | Ordering::Equal) => Ordering::Less,
+            }
+        }).is_ok()
+    }
+
+    fn ranges(&self) -> &[Range<usize>] {
+        &self.0
     }
 }
 
 #[aoc(day5, part1)]
 fn part1(input: &str) -> usize {
-    let db = DB::new(input);
+    let mut lines = input.lines();
+    let fresh_ingredients: Vec<Range<usize>> = lines.by_ref().take_while(|line| !line.is_empty())
+        .map(|line| {
+            let (start, end) = line.split_once('-').unwrap();
+            start.parse().unwrap()..end.parse::<usize>().unwrap()+1
+        })
+        .collect();
+    let fresh_ingredients = IntervalSet::new(fresh_ingredients);
 
-    db.available_ingredients
+    let available_ingredients: Vec<usize> = lines
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    available_ingredients
         .iter()
         .copied()
-        .filter(|ingredient| {
-            db.fresh_ingredients
-                .iter()
-                .any(|range| range.contains(ingredient))
+        .filter(|&ingredient| {
+            fresh_ingredients.contains(ingredient)
         })
         .count()
 }
 
 #[aoc(day5, part2)]
 fn part2(input: &str) -> usize {
-    let db = DB::new(input);
-    let mut fresh_ranges = db.fresh_ingredients;
+    let fresh_ranges: Vec<Range<usize>> = input.lines().take_while(|line| !line.is_empty())
+        .map(|line| {
+            let (start, end) = line.split_once('-').unwrap();
+            start.parse().unwrap()..end.parse::<usize>().unwrap()+1
+        })
+        .collect();
+    let intervalset = IntervalSet::new(fresh_ranges);
 
-    fresh_ranges.sort_by_key(|range| (*range.start(), *range.end()));
-
-    let mut merged_ranges = Vec::new();
-
-    let mut r = fresh_ranges[0].clone();
-    for fresh_range in &fresh_ranges[1..] {
-        if *r.end() + 1 < *fresh_range.start() {
-            merged_ranges.push(r);
-            r = fresh_range.clone();
-        } else if fresh_range.end() > r.end() {
-            r = *r.start()..=*fresh_range.end()
-        }
-    }
-    merged_ranges.push(r);
-
-    merged_ranges
-        .into_iter()
-        .map(|range| *range.end() - *range.start() + 1)
+    intervalset.ranges().iter()
+        .map(|range| range.len())
         .sum()
 }
